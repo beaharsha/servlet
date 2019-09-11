21bb49b1ef90640057298ebe9a1afb2fd500d27d
beaharsha <55123614+beaharsha@users.noreply.github.com>
2019-09-11 09:01:04 +0530

    added TS and Kerala current bill servlets

--- src/main/java/com/wallet/paybill/servlet/CalclulateElectricityBillServlet.java
+++ src/main/java/com/wallet/paybill/servlet/CalclulateElectricityBillServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class CalclulateElectricityBillServlet  extends HttpServlet{
+public class CalclulateElectricityBillServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -16,20 +16,21 @@ public class CalclulateElectricityBillServlet  extends HttpServlet{
 		Integer currentReading = Integer.valueOf(req.getParameter("current"));
 		String state = req.getParameter("state");
 		String type = req.getParameter("type");
-		
-		Float rate = (Float) req.getAttribute("rate");
+
+		Integer rate = (Integer) req.getAttribute("rate");
 		int reading = currentReading - previousReading;
-		
-		Float amount = reading * rate;
-		
+
+		Integer amount = reading * rate;
+
 		resp.setContentType("text/html");
 		PrintWriter writer = resp.getWriter();
-		
+
 		writer.append("Previous reading is = ").append(previousReading.toString()).append("<br>")
-		.append("Current reading is = ").append(currentReading.toString()).append("<br>").append("Rate for unit = " )
-		.append(rate.toString()).append("<br>").append("Bill per this month is = ").append(amount.toString())
-		.append("<br>");
+				.append("Current reading is = ").append(currentReading.toString()).append("<br>")
+				.append("State is = ").append(state).append("<br>")
+				.append("Rate for unit = ").append(rate.toString()).append("<br>").append("Bill per this month is = ")
+				.append(amount.toString()).append("<br>");
 		;
-		
+
 	}
 }
---
+++ src/main/java/com/wallet/paybill/servlet/TSPayElectricityServlet.java
@@ -0,0 +1,35 @@
+package com.wallet.paybill.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TSPayElectricityServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String type = req.getParameter("type");
+		Integer ratePerUnit = 0;
+		ratePerUnit = Integer.valueOf(getServletConfig().getInitParameter("ratePerUnit")); // reading config values
+
+		if (req.getParameter("type").equalsIgnoreCase("commercial")) {
+			ratePerUnit = Integer.valueOf(getServletContext().getInitParameter("ratePerUnitCommericial"));
+		}
+		req.setAttribute("rate", ratePerUnit);
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("calculate");
+		dispatcher.include(req, resp);
+
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+
+		writer.append("Please pay ").append(type).append(" bill online or offiline before 20th of this month");
+	}
+
+}
